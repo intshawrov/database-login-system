26d71366ec49cb69903bca838ffd2732599f78fa
Shawrov <intshawrov@gmail.com>
2026-01-03 17:20:31 +0600

    updated

--- index.php
+++ index.php
@@ -3,19 +3,18 @@
 <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Database-Login</title>
+      <title>database-login</title>
       <link rel="stylesheet" href="style.css">
 </head>
-<body>
 
 <div class="container">
-      <div class="form-container">
-            <div class="form-toggle">
-                  <button id="login" class="active">Login</button>
-                  <button id="register">Register</button>
-            </div>
+      <div class="form">
+            <button class="btn" id="login">Login</button>
+            <button id="register">Register</button>
       </div>
 </div>
+
+<body>
       
 </body>
 </html>

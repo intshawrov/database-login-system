00a6fcd0950c67a4c725b92ee2427dfe0edb18d3
Shawrov <intshawrov@gmail.com>
2026-01-05 19:35:19 +0600

    Updated

---
+++ backend/parts/footer.php
@@ -0,0 +1,19 @@
+<footer>
+  <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</footer>
